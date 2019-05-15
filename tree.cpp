d3d7f0c030dc7276b4350e96f75ce4b53eccdee4
eolico910201 <35620768+eolico910201@users.noreply.github.com>
2019-05-15 13:05:59 +0800

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -1,8 +1,33 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
+struct node
+{
+    int num,id;
+    node *left,*right;
+};
+node *tree;
+bool cmp(node a,node b)
+{
+    return a.num<b.num;
+}
+void input(int n)
+{
+    tree=new node[n];
+    int i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&tree[i].num);
+        tree[i].id=i;
+        tree[i].left=tree[i].right=NULL;
+    }
+    sort(tree,tree+n,cmp);
+}
+
+
 int main()
 {
-
-  return 0;
+    
+    return 0;
 }
 
